a9a98b7c78ec2a88d610f44d2b3f4bc86f850dee
tofan79 <nanasmuda.404@outlook.co.id>
2026-06-23 17:47:34 +0700

    fix: add Headset Mic Boost 1 to prevent fluctuation

--- fix-asus-audio.sh
+++ fix-asus-audio.sh
@@ -3,6 +3,7 @@ CARD=$(grep -l 'ALC256' /proc/asound/card*/codec* | head -1 | grep -oP 'card\K\d
 [[ -z $CARD ]] && exit 0
 amixer -c "$CARD" sset 'Capture' 45 unmute
 amixer -c "$CARD" sset 'Internal Mic Boost' 1
+amixer -c "$CARD" sset 'Headset Mic Boost' 1
 amixer -c "$CARD" sset 'Speaker' 87 unmute
 amixer -c "$CARD" sset 'Headphone' 87 unmute
 amixer -c "$CARD" sset 'Master' 87 unmute
